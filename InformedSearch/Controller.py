09aca1bffeda77c705eabb89aa510a98bf616057
Brayan Diaz <brayandiazssjd@gmail.com>
2024-09-11 21:55:40 -0500

    Updated HEAD to last changes.

--- InformedSearch/Controller.py
+++ InformedSearch/Controller.py
@@ -1,61 +1,82 @@
 from CityController import CityController
-
+import heapq
 
 class Controller:
 
-    def __init__(self, matrix, ct: CityController):
-        self.__matrix = matrix
-        self.__ct = ct
+	def __init__(self, matrix, ct: CityController):
+		self.__matrix = matrix
+		self.__ct = ct
 
-        """
-        #Recursiv
-        def enrute(self, origin, destine, visited):
-                if origin == destine:
-                        return [destine]
-                next_one = self.__next(origin, destine, visited)
+	# Returna el par solución [[ids], [distances]] para A
+	# TODO: optimize using a binary passed list
+	# TODO: change return to [[cities: City], distance:int]
+	def enruteA(self, origin, destine): 
+		diss = []
+		passed = []
+		ids = []		
+		while origin != destine:
+			for neigh in self.__matrix[origin]:
+				if neigh[0] not in passed:  # or neigh[0] == origin
+					ids.append(neigh[0])
+					passed.insert(0, neigh[0])
+					diss.append(neigh[1])
+					origin = neigh[0]
+					break
+		return [ids, diss]
 
-                return [origin] + self.enrute(next_one,destine, visited.append(origin))
+	# Returna el par solución [[ids], [distances]] para A*
+	# TODO: optimize using a binary
+	# TODO: change return to [[cities: City], distance:int]
+	# TODO: change index for sub list "[:]"
+	def enruteAA(self, origin: int, destiny: int):
+		ids = [origin]
+		passed = [origin]
+		diss = []
+		
+		while origin != destiny:
+			neigh = self.__matrix[origin][0]
+			# nearer = [neighbour, distance from origin to neighbour]
+			nearer = [neigh,	self.__ct.idistance(neigh[0], destiny)]
+			for i in range(1, len(self.__matrix[origin])):
+				neigh = self.__matrix[origin][i]
+				distance = self.__ct.idistance(neigh[0], destiny)
 
-        def __next(self, origin, destine, visited):
-                for neigh in self.matrix[origin]:
-                        if neigh[0] not in visited or neigh[0] == destine:
-                                return neigh
-                return -1
-        """
+				if neigh[0] not in passed:
+					passed.insert(0, neigh[0])
+					if (distance + neigh[1]) < nearer[0][1] + nearer[1]:
+						nearer = [neigh, distance]
+			origin = nearer[0][0]
+			ids.append(nearer[0][0])
+			diss.append(nearer[0][1])
+		return [ids, diss] 
 
-    def enruteA(self, origin, destine):
-        route = []
-        passed = []
-        while origin != destine:
-            # TODO: eliminar este condicional, no es necesario
-            if not self.__matrix[origin]:  # Verifica si la lista es vacía o None
-                print(f"Error: No hay vecinos disponibles para la ciudad con id {origin}.")
-                return route
+	def lazy_prim(self):
+		mst = []
+		count, cost = 0, 0
+		n = len(self.__matrix)
+		visited = [False for i in range(n)]
+		pq = []
 
-            for neigh in self.__matrix[origin]:
-                if neigh[0] not in passed:  # or neigh[0] == origin
-                    route.append(neigh)
-                    passed.insert(0, neigh[0])
-                    origin = neigh[0]
-                    break
-        return route
+		while not pq and count < n:
+			edge = heapq.heappop(pq)
+			index = 
 
-    def enruteAA(self, origin: int, destiny: int):
-        route = [[origin, 0]]
-        passed = [origin]
-        while origin != destiny:
-            nearer = [self.__matrix[origin][0][0], self.__ct.idistance(self.__matrix[origin][0][0], destiny), self.__matrix[origin][0][1]]
-            for i in range(1, len(self.__matrix[origin])):
-                neigh = self.__matrix[origin][i]
-                print("$",neigh[0] not in passed)
-                distance = self.__ct.idistance(neigh[0], destiny)
-                if neigh[0] not in passed:
-                    passed.insert(0, neigh[0])
-                    
-                    if (distance + neigh[1]) < nearer[1] + nearer[2]:
-                        nearer = [neigh[0], distance, neigh[1]]
-                
-            origin = nearer[0]
-            route.append([nearer[0], nearer[2]])
-            print(passed)
-        return route
+
+		return [mst, cost]
+
+	def fast_prim(self):
+		mst = []
+		n = len(self.__matrix)-1
+
+
+		return mst
+
+	def minor(self):
+		minor = self.__matrix[0][0]
+		for c in self.__matrix[1:]:
+			if c[0][1] < minor[1]:
+				minor = c[0]
+		return minor
+
+	def __addEdges(self, index):
+		pass
